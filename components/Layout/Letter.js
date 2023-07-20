9c65da3c6e7b21fcce5ddc00d339b76ff9af6715
Mujeeb Cpy <mujeebcpy@gmail.com>
2023-07-20 10:29:10 +0530

    changed malayalam font

--- components/Layout/Letter.js
+++ components/Layout/Letter.js
@@ -4,7 +4,7 @@ const Letter = () => {
   return (
     <section id="letter" className="flex lg:hidden">
       <div className="flex flex-col text-center my-auto text-white px-8  pt-28 justify-end">
-        <p>
+        <p className="font-manjari">
           “സുഹൃത്തേ, <br />ഈ വരുന്ന ഓഗസ്റ്റ് 18 ന് ഞങ്ങൾ സ്പെഷ്യൽ മാരേജ് ആക്ട്
           പ്രകാരം വിവാഹിതരാവുകയാണ്. തുടർന്ന് ഓഗസ്റ്റ് 20 ന് വട്ടേക്കാട് ആത്രേയ
           കല്ല്യാണമഹലിൽ വെച്ച് പകൽ 11.00 മുതൽ 2.00 മണിവരെ നടക്കുന്ന
--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
         >
           <div className="flex flex-col text-white px-8 pb-20 pt-28 justify-end">
             <div className="flex flex-col text-center my-auto">
-              <p>
+              <p className="font-manjari">
                 “സുഹൃത്തേ, <br />ഈ വരുന്ന ഓഗസ്റ്റ് 18 ന് ഞങ്ങൾ സ്പെഷ്യൽ മാരേജ്
                 ആക്ട് പ്രകാരം വിവാഹിതരാവുകയാണ്. തുടർന്ന് ഓഗസ്റ്റ് 20 ന്
                 വട്ടേക്കാട് ആത്രേയ കല്ല്യാണമഹലിൽ വെച്ച് പകൽ 11.00 മുതൽ 2.00
